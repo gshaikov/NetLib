cac9efd6dcdecdc962e5d03e486d1cd868e4609f
George <g.shaikov@gmail.com>
2017-11-08 21:43:28 +0100

    changed data cleaning and feature engineering - got better normalization and less bias, so it doesn't get stuck in local min

--- clean_data.py
+++ clean_data.py
@@ -8,13 +8,10 @@ Clean dataset
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set(style="whitegrid", color_codes=True)
-
-
-#%%
-#####################################################
 
 
 RANK_DICT = {
@@ -78,98 +75,13 @@ def load_data():
     return df
 
 
-def age_group(x):
-    '''age_group'''
-    if x['Title'] == 'Master' or x['Age'] < 16:
-        return 'young'
-    else:
-        return 'old'
-
-
-def create_discrete_features(df):
-    '''create_discrete_features'''
-    descrete_cols = [
-        'AgeGroup',
-        'Relationship',
-        'Rank',
-        'Sex',
-        'Pclass',
-        'Deck',
-    ]
-
-    df_discrete_features = pd.DataFrame()
-
-    for name in descrete_cols:
-        for category in set(df[name].values):
-            df_discrete_features["{}_{}".format(
-                name, category)] = (df[name] == category)
-
-    df_discrete_features = df_discrete_features.astype('int32')
-
-    df = df.drop(descrete_cols, 1)
-
-    df = pd.concat([df, df_discrete_features], axis=1)
-
-    return df
-
-
-def create_datasets(df, split=0.7):
-    '''create_datasets'''
-    assert isinstance(split, float)
-
-    m_train = round(df[df['Train'] == 1].shape[0] * split)
-    m_val = round(df[df['Train'] == 1].shape[0] - m_train)
-    m_test = round(df[df['Train'] == 0].shape[0])
-
-    ms = [m_train, m_val, m_test]
-
-    df_train = df.loc[:m_train - 1, :].copy()
-    df_val = df.loc[m_train:m_train + m_val - 1, :].copy()
-    df_test = df.loc[m_train + m_val:, :].copy()
-
-    dfs = [df_train, df_val, df_test]
-
-    y_train = df_train['Survived'].copy()
-    y_val = df_val['Survived'].copy()
-
-    ys = [y_train, y_val, None]
-
-    for idx, item in enumerate(dfs):
-        dfs[idx] = item.drop(['Survived', 'Train'], 1)
-
-    return dfs, ys, ms
-
-
-def normalize_dataset(df_train, dfs):
-    '''normalize_dataset'''
-    normalize_cols = {
-        'Age',
-        'Fare',
-        'SibSp',
-        'Parch',
-    }
-
-    for col in normalize_cols:
-
-        # find mean and variance of the feature column in training dataset
-        mean_val = np.mean(df_train[col])
-        var_val = np.var(df_train[col])
-
-        # normalize the feature column in all datasets
-        for idx, df in enumerate(dfs):
-            df[col] = (df[col] - mean_val) / np.sqrt(var_val)
-            dfs[idx] = df
-
-    return dfs
-
-
-#%%
-#####################################################
-
-
-if __name__ == '__main__':
+def feature_engineering():
     # Load data
     df = load_data()
+
+    # shuffle dataframe 5 times
+    for _ in range(5):
+        df = df.sample(frac=1).reset_index(drop=True)
 
     # Data cleaning, feature engineerng
     titles = df['Name'].apply(lambda x: x
@@ -194,21 +106,50 @@ if __name__ == '__main__':
         df['Title'].apply(lambda x: RELATIONSHIP_DICT.get(x, 'NA'))
     )
 
+    def age_group(x):
+        '''age_group'''
+        if x['Title'] == 'Master' or x['Age'] < 15:
+            return 'kid'
+        elif x['Age'] < 30:
+            return 'young'
+        elif x['Age'] < 60:
+            return 'adult'
+        elif x['Age'] > 60:
+            return 'old'
+        else:
+            return 'NA'
+
     df.insert(
-        df.columns.get_loc('Title') + 1,
+        df.columns.get_loc('Age') + 1,
         'AgeGroup',
         df.apply(age_group, axis=1)
     )
 
+    def fare_group(x):
+        '''fare_group'''
+        if x['Fare'] > 0:
+            return np.ceil(x['Fare'] / 10.0) * 10.0
+
     n_tickets = df.groupby('Ticket').apply(lambda x: x.shape[0])
     df['Fare'] = df.apply(lambda x: x['Fare'] / n_tickets[x['Ticket']], axis=1)
+
+    fare_avg = df[df['Fare'] > 0].groupby('Pclass')['Fare'].mean()
+
+    df['Fare'] = df.apply(lambda x: fare_avg[x['Pclass']]
+                          if not x['Fare'] > 0 else x['Fare'], axis=1)
+
+    df.insert(
+        df.columns.get_loc('Fare') + 1,
+        'FareGroup',
+        df.apply(fare_group, axis=1)
+    )
 
     def what_cabin(x):
         if not pd.isnull(x):
             if x[0] in cabin_list:
                 return x[0]
         else:
-            return 'unknown'
+            return 'NA'
 
     cabin_list = ['A', 'B', 'C', 'D', 'E', 'F', 'T', 'G']
     df['Deck'] = df['Cabin'].apply(what_cabin)
@@ -235,27 +176,147 @@ if __name__ == '__main__':
     married = set(df.apply(check_married, axis=1))
     df = df.apply(is_married, axis=1)
 
-    #sns.swarmplot(x="Status", y="Age", hue="Survived", data=df, split=True)
-    #sns.violinplot(x="Status", y="Pclass", hue="Survived", data=df, split=True)
+    return df
 
-    df = create_discrete_features(df)
 
+def feature_dropping(df):
+    # drop some columns
     drop_cols = [
         'PassengerId',
-        'Title',
+        # 'Pclass',
         'Name',
+        'Title',
+        # 'Relationship',
+        # 'Rank',
+        # 'Sex',
+        # 'Age',
+        'AgeGroup',
+        # 'SibSp',
+        # 'Parch',
         'Ticket',
+        # 'Fare',
+        'FareGroup',
         'Cabin',
-        'Embarked',
+        # 'Deck',
+        # 'Embarked',
     ]
-
-    df_id = df[['PassengerId', 'Train', 'Survived']]
 
     df = df.drop(drop_cols, 1)
 
+    # deal with nan values
     for col in df.columns:
         if df[col].isnull().values.any():
-            df[col] = df[col].fillna(df[col].mean())
+            if col == 'Age':
+                df[col] = df[col].fillna(df[col].mean())
+            else:
+                df[col] = df[col].fillna('NA')
+
+    return df
+
+
+def create_discrete_features(df):
+    '''create_discrete_features'''
+    descrete_cols = [
+        'Relationship',
+        'Rank',
+        'Sex',
+        'AgeGroup',
+        'Embarked',
+        'Deck',
+    ]
+
+    df_discrete_features = pd.DataFrame()
+
+    for name in descrete_cols:
+        if name in df.columns:
+            for category in set(df[name].values):
+                df_discrete_features["{}_{}".format(
+                    name, category)] = (df[name] == category)
+
+    df_discrete_features = df_discrete_features.astype('int32')
+
+    df = df.drop(descrete_cols, 1, errors='ignore')
+
+    df = pd.concat([df, df_discrete_features], axis=1)
+
+    return df
+
+#%%
+#####################################################
+
+
+if __name__ == '__main__':
+    df = feature_engineering()
+
+#    df_plot = df[df['Train']==1]
+
+#    sns.swarmplot(x="Pclass", y="Age", hue="Survived", data=df_plot, split=False)
+#    sns.violinplot(x="Status", y="Pclass", hue="Survived", data=df, split=True)
+
+#    plt.figure()
+#    plt.scatter(x=df_plot['Age'], y=df_plot['Fare'], c=df_plot['Survived'])
+#    plt.show()
+
+    # create separate dataset with IDs
+    df_id = df[['PassengerId', 'Train', 'Survived']]
+
+    df = feature_dropping(df)
+
+    df = create_discrete_features(df)
 
     df.to_csv('dataset/df.csv', index=False)
     df_id.to_csv('dataset/df_id.csv', index=False)
+
+
+#%%
+#####################################################
+
+def create_datasets(df, split=0.7):
+    '''create_datasets'''
+    assert isinstance(split, float)
+
+    m_train = round(df[df['Train'] == 1].shape[0] * split)
+    m_val = round(df[df['Train'] == 1].shape[0] - m_train)
+    m_test = round(df[df['Train'] == 0].shape[0])
+
+    ms = [m_train, m_val, m_test]
+
+    df_train = df[df['Train'] == 1].iloc[:m_train, :].copy()
+    df_val = df[df['Train'] == 1].iloc[m_train:, :].copy()
+    df_test = df[df['Train'] == 0].copy()
+
+    dfs = [df_train, df_val, df_test]
+
+    y_train = df_train[['Survived']].copy()
+    y_val = df_val[['Survived']].copy()
+
+    ys = [y_train, y_val, pd.DataFrame()]
+
+    for idx, item in enumerate(dfs):
+        dfs[idx] = item.drop(['Survived', 'Train'], 1)
+
+    return dfs, ys, ms
+
+
+def normalize_dataset(dfs):
+    '''normalize_dataset'''
+    cols_skewed = [
+        'Fare',
+    ]
+
+    dfs[0][cols_skewed] = dfs[0][cols_skewed].apply(np.log, axis=0)
+
+    df_train = dfs[0]
+
+    for col in df_train.columns:
+        # find mean and SD of the feature column in training dataset
+        mean_val = np.mean(df_train[col])
+        # sd_val = np.std(df_train[col])
+        delta_val = np.max(df_train[col]) - np.min(df_train[col])
+
+        # normalize the feature column in all datasets
+        for idx, df in enumerate(dfs):
+            df[col] = (df[col] - mean_val) / delta_val
+            dfs[idx] = df
+
+    return dfs
--- nn.py
+++ nn.py
@@ -10,15 +10,13 @@ from clean_data import create_datasets, normalize_dataset
 from myneuralnetapi import Layer, Input, Data
 from myneuralnetapi import BinaryClassifierNetwork
 
-import tensorflow as tf
-from tensorflow.python.framework import ops
+#import tensorflow as tf
+#from tensorflow.python.framework import ops
 
 import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
-
-import seaborn as sns
 
 import logging
 
@@ -37,14 +35,14 @@ def main():
     dataset_ids = pd.read_csv('dataset/df_id.csv')
 
     # Split into training, validation, and test datasets
-    datasets, labels, sizes = create_datasets(dataset_main, split=0.8)
+    datasets, labels, sizes = create_datasets(dataset_main, split=0.7)
 
     # check that dataset was split correctly
     assert sum(list(map((lambda x: x.shape[0]), datasets))) \
         == dataset_main.shape[0]
 
     # Normalize
-    datasets = normalize_dataset(datasets[0], datasets)
+    datasets = normalize_dataset(datasets)
 
     # check that dataset was split correctly
     assert sum(list(map((lambda x: x.shape[0]), datasets))) \
@@ -78,10 +76,12 @@ def main():
     net.grad_check()
 
     net.add_input(Input(train_n_features))
-    net.add_layer(Layer('hidden', 'relu', 20, train_n_features))
-    net.add_layer(Layer('hidden', 'relu', 10, 20))
-#    net.add_layer(Layer('hidden', 'relu', train_n_features, train_n_features))
-    net.add_output(Layer('output', 'sigmoid', 1, 10))
+
+    net.add_layer(Layer('hidden', 'relu', train_n_features, train_n_features))
+    net.add_layer(Layer('hidden', 'relu', train_n_features, train_n_features))
+    net.add_layer(Layer('hidden', 'relu', train_n_features, train_n_features))
+
+    net.add_output(Layer('output', 'sigmoid', 1, train_n_features))
 
     net.show_network()
 
@@ -89,10 +89,11 @@ def main():
     # Hyperparameters
 
     epochs = 100000
-    learn_rate = 0.1
-    learn_decay = 1 / 10000
 
-    lambd = 1.0
+    learn_rate = 0.1
+    learn_decay = 1 / 10000 * 0
+
+    lambd = 1.0 * 0
     threshold = 0.5
 
     ###############################
@@ -223,24 +224,23 @@ def main():
         learn_rate_vec = np.logspace(
             start=-3,
             stop=0,
-            num=4,
+            num=12,
             dtype=np.float32,
         )
 
         # array of var lambdas (regularization)
         lambd_vec = np.logspace(
-            start=-2,
-            stop=0,
-            num=5,
+            start=-3,
+            stop=1,
+            num=16,
             dtype=np.float32,
         )
 
         # prediction results will be collected here
         results_table = pd.DataFrame()
 
-        for learn_rate in learn_rate_vec:
-            for lambd in lambd_vec:
-
+        for lambd in lambd_vec:
+            for learn_rate in learn_rate_vec:
                 # train routine
                 cost_list = net.train(
                     input_data=dataset_train,
@@ -277,99 +277,8 @@ def main():
     ###############################
     # Use tensorflow
 
-    if USE_TENSORFLOW_INSTEAD == True:
-        ops.reset_default_graph()
-
-        X = tf.placeholder(
-            dtype=tf.float32, shape=(train_n_features, None), name='X')
-        Y = tf.placeholder(
-            dtype=tf.float32, shape=(1, None), name='Y')
-
-        tf.set_random_seed(1)
-
-        seed = 1
-
-        W1 = tf.get_variable(
-            "W1", [20, train_n_features], initializer=tf.contrib.layers.xavier_initializer(seed=seed))
-        b1 = tf.get_variable(
-            "b1", [20, 1], initializer=tf.zeros_initializer())
-        W2 = tf.get_variable(
-            "W2", [10, 20], initializer=tf.contrib.layers.xavier_initializer(seed=seed))
-        b2 = tf.get_variable(
-            "b2", [10, 1], initializer=tf.zeros_initializer())
-        W3 = tf.get_variable(
-            "W3", [1, 10], initializer=tf.contrib.layers.xavier_initializer(seed=seed))
-        b3 = tf.get_variable(
-            "b3", [1, 1], initializer=tf.zeros_initializer())
-
-        parameters = {
-            "W1": W1,
-            "b1": b1,
-            "W2": W2,
-            "b2": b2,
-            "W3": W3,
-            "b3": b3,
-        }
-
-        Z1 = tf.add(tf.matmul(W1, X), b1)
-        A1 = tf.nn.relu(Z1)
-        Z2 = tf.add(tf.matmul(W2, A1), b2)
-        A2 = tf.nn.relu(Z2)
-        Z3 = tf.add(tf.matmul(W3, A2), b3)
-
-        logits = tf.transpose(Z3)
-        labels = tf.transpose(Y)
-
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-            logits=logits, labels=labels))
-
-        optimizer = tf.train.AdamOptimizer(
-            learning_rate=learn_rate).minimize(cost)
-
-        init = tf.global_variables_initializer()
-
-        seed = 3
-
-        costs = []
-
-        with tf.Session() as sess:
-            sess.run(init)
-
-            total_cost = 0.
-
-            for epoch in range(epochs):
-                _, curr_cost = sess.run(
-                    [optimizer, cost],
-                    feed_dict={X: datasets[0], Y: labels[0]})
-
-                total_cost += curr_cost
-
-                if epoch % 100 == 0:
-                    print("Cost after epoch %i: %f" % (epoch, curr_cost))
-                if epoch % 100 == 0:
-                    costs.append(curr_cost)
-
-            # plot the cost
-            plt.plot(np.squeeze(costs))
-            plt.ylabel('cost')
-            plt.xlabel('iterations (per tens)')
-            plt.title("Learning rate =" + str(learn_rate))
-            plt.show()
-
-            # lets save the parameters in a variable
-            parameters = sess.run(parameters)
-            print("Parameters have been trained!")
-
-            # Calculate the correct predictions
-            correct_prediction = tf.equal(tf.argmax(Z3), tf.argmax(Y))
-
-            # Calculate accuracy on the test set
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-
-            print("Train Accuracy:", accuracy.eval(
-                {X: datasets[0], Y: labels[0]}))
-            print("Test Accuracy:", accuracy.eval(
-                {X: datasets[2], Y: labels[2]}))
+    if USE_TENSORFLOW_INSTEAD is True:
+        pass
 
     return results_table
 
@@ -379,11 +288,12 @@ def main():
 
 if __name__ == '__main__':
     TRAIN_NETWORK = False
-    CHECK_BIAS_VARIANCE = False
-    CHECK_HYPERPARAMETERS = False
     GENERATE_PREDICTIONS = False
 
-    USE_TENSORFLOW_INSTEAD = True
+    CHECK_BIAS_VARIANCE = False
+    CHECK_HYPERPARAMETERS = True
+
+    USE_TENSORFLOW_INSTEAD = False
 
     if GENERATE_PREDICTIONS and not TRAIN_NETWORK:
         TRAIN_NETWORK = True
